cae5ab7a2449f9da6049f141794db571e4a3da79
Andrea <andreapinchera.1989@gmail.com>
2024-12-20 18:19:46 +0100

    03-B done

--- 2024/3/03-B.py
+++ 2024/3/03-B.py
@@ -26,19 +26,23 @@ with open(script_run + "/03-A_inputlist.txt", "r") as the_file:
     for line in the_file:
         puzzle_input += line.strip()
 
-exp = re.compile(r"(don't\(\)|do\(\))?.?(mul\(\d+,\d+\))")
-numbers_exp = re.compile(r'(\d*),(\d*)')
+exp = re.compile(r"(don't\(\)|do\(\)|mul\(\d+,\d+\))")
+numbers_exp = re.compile(r'mul\((\d{1,3}),(\d{1,3})\)')
 
 #parse puzzle input and store useful instructions into list
 mul_instructions = re.findall(exp, puzzle_input)
 
 add_up = 0
 doing = True
+
 for i in mul_instructions:
-    numbers_to_multiply = re.findall(numbers_exp, i[1])
-    doing = switch(doing, i[0])
-    print(doing, i)
-    #print(doing)
-    if doing == True : add_up += mul(int(numbers_to_multiply[0][0]), int(numbers_to_multiply[0][1])) 
-    #print("-------------------------------------")
+    print(i)
+    if i == "don't()" or i == "do()":
+        doing = switch(doing, i)
+    else:
+        numbers_to_multiply = re.match(numbers_exp, i)
+        print(doing)
+        if doing == True:
+            add_up += mul(int(numbers_to_multiply.group(1)), int(numbers_to_multiply.group(2)))
+
 print(add_up)
