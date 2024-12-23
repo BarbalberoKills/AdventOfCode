ec0b3ef33d8693183e4b4055183e1c04b334d607
Andrea <andreapinchera.1989@gmail.com>
2024-12-23 17:57:20 +0100

    better compact version for 4-A

--- 2024/4/04-A.py
+++ 2024/4/04-A.py
@@ -1,19 +1,6 @@
 import pathlib
 
 
-script_run = str(pathlib.Path(__file__).parent.resolve())
-puzzle_input = ""
-with open(script_run + "/04-A_inputlist.txt", "r") as the_file:
-    for line in the_file:
-        puzzle_input += line
-
-mat = []
-for c, line in enumerate(puzzle_input.splitlines()):
-    mat.append([])
-    for k, element in enumerate(line):
-        mat[c].append(element)
-
-
 def move(pos, dir, increment):
     match(dir):
         case("right"):
@@ -44,93 +31,34 @@ def move(pos, dir, increment):
             pos[0] -= abs(increment)
             pos[1] += abs(increment)
             return pos
+        
+
+script_run = str(pathlib.Path(__file__).parent.resolve())
+puzzle_input = ""
+with open(script_run + "/04-A_inputlist.txt", "r") as the_file:
+    for line in the_file:
+        puzzle_input += line
 
 
+mat = []
+for c, line in enumerate(puzzle_input.splitlines()):
+    mat.append([])
+    for k, element in enumerate(line):
+        mat[c].append(element)
 
-total_words = []
+
+directions = ["right", "bottom-right", "bottom", "bottom-left", "left", "up-left", "up", "up-right"]
+word_to_find = "XMAS"
+xmas_counter = 0
 for actual_row in range(len(mat)):
     for actual_col in range(len(mat[actual_row])):
-        p = mat[actual_row][actual_col]
-        size_word = 4
-        word = ""
-        #print("RIGHT", p)
-        for lenght_word in range(size_word):
-            if actual_col + size_word <= len(mat[actual_row]):
-                next_pos = move([actual_row, actual_col], "right", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("BOTTOM-RIGHT", p)
-        for lenght_word in range(size_word):
-            if (actual_col + size_word <= len(mat[actual_row])) and (actual_row + size_word <= len(mat)):
-                next_pos = move([actual_row, actual_col], "bottom-right", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("BOTTOM", p)
-        for lenght_word in range(size_word):
-            if actual_row + size_word <= len(mat):
-                next_pos = move([actual_row, actual_col], "bottom", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("BOTTOM-LEFT", p)
-        for lenght_word in range(size_word):
-            if (actual_col - 3 >= 0) and (actual_row + size_word <= len(mat)):
-                next_pos = move([actual_row, actual_col], "bottom-left", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("LEFT", p)
-        for lenght_word in range(size_word):
-            if actual_col - 3 >= 0:
-                next_pos = move([actual_row, actual_col], "left", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("UP-LEFT", p)
-        for lenght_word in range(size_word):
-            if (actual_col - 3 >= 0) and (actual_row - size_word >= 0):
-                next_pos = move([actual_row, actual_col], "up-left", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("UP", p)
-        for lenght_word in range(size_word):
-            if actual_row - size_word >= 0:
-                next_pos = move([actual_row, actual_col], "up", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-        #print("UP-RIGHT", p)
-        for lenght_word in range(size_word):
-            if (actual_col + size_word <= len(mat[actual_row])) and (actual_row - 4 >= 0):
-                next_pos = move([actual_row, actual_col], "up-right", lenght_word)
-                #print(next_pos)
-                word += str(mat[next_pos[0]][next_pos[1]])
-        if word != "": total_words.append(word)
-        word = ""
-        #print("------------------------------------")
-#print(total_words)
-
-
-xmas_counter = 0
-for word in total_words:
-    if (''.join(word) == "XMAS"):
-        xmas_counter +=1
-
+        for dir in directions:
+            next_pos = move([actual_row, actual_col], dir, len(word_to_find))
+            if (0 <= next_pos[0] <= len(mat[actual_row])) and (0 <= next_pos[1] <= len(mat)):
+                word = ""
+                for position_in_word in range(len(word_to_find)):
+                    next_pos = move([actual_row, actual_col], dir, position_in_word)
+                    word += str(mat[next_pos[0]][next_pos[1]])
+                if word == "XMAS":
+                    xmas_counter += 1
 print(xmas_counter)
