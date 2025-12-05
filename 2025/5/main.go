d189b183b64104adee07215ce39b478b0ab90a51
Andrea <andreapinchera.1989@gmail.com>
2025-12-05 16:53:07 +0100

    feat: add day6

--- 2025/5/main.go
+++ 2025/5/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/BarbalberoKills/AdventOfCode/2025/5/p1"
+	"github.com/BarbalberoKills/AdventOfCode/2025/5/p2"
 )
 
 func main() {
-	fmt.Printf("Solution part1: %d\n", p1.Solve("./input.txt"))
-	// fmt.Printf("Solution part2: %d\n", p2.Solve("./input.txt"))
+	// fmt.Printf("Solution part1: %d\n", p1.Solve("./input.txt"))
+	fmt.Printf("Solution part2: %d\n", p2.Solve("./input_test.txt"))
 }
