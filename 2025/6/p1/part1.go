f48f36280bb08f58ec76a5e7f8e72cf4b3b43084
Andrea <andreapinchera.1989@gmail.com>
2025-12-06 14:24:01 +0100

    2025/06-A resolved

--- 2025/6/p1/part1.go
+++ 2025/6/p1/part1.go
@@ -3,9 +3,8 @@ package p1
 import (
 	"bufio"
 	"fmt"
-	"slices"
+	"regexp"
 	"strconv"
-	"strings"
 
 	"github.com/BarbalberoKills/AdventOfCode/2025/utils"
 )
@@ -17,43 +16,70 @@ func Solve(input string) int {
 	}
 	defer file.Close()
 
-	var availableIngredients int
+	numbers, operations := parseFile(scanner)
+	numbers = transpose(numbers)
 
-	ranges, ingredients := parseFile(scanner)
-	availableIngredients = len(freshCheck(ranges, ingredients))
-
-	return availableIngredients
-}
-
-func parseFile(scanner *bufio.Scanner) ([]string, []string) {
-	ranges, ingredients := []string{}, []string{}
-	for scanner.Scan() {
-		line := scanner.Text()
-		if strings.Contains(line, "-") {
-			ranges = append(ranges, line)
-		} else if strings.Contains(line, "\n") {
-			continue
-		} else {
-			ingredients = append(ingredients, line)
-		}
-	}
-	return ranges, ingredients
-
-}
-
-func freshCheck(ranges, ingredients []string) []int {
-	checkedIng := []int{}
-	for _, interval := range ranges {
-		fmt.Printf("Checking range: %v\n", interval)
-		startS, endS := strings.Split(interval, "-")[0], strings.Split(interval, "-")[1]
-		start, _ := strconv.Atoi(startS)
-		end, _ := strconv.Atoi(endS)
-		for _, ingredientS := range ingredients {
-			ingredient, _ := strconv.Atoi(ingredientS)
-			if start <= ingredient && ingredient <= end && !slices.Contains(checkedIng, ingredient) {
-				checkedIng = append(checkedIng, ingredient)
+	total := 0
+	for i, o := range operations {
+		parzial := 0
+		if o == "+" {
+			for _, n := range numbers[i] {
+				num, _ := strconv.Atoi(n)
+				parzial = parzial + num
+			}
+		} else if o == "*" {
+			for _, n := range numbers[i] {
+				num, _ := strconv.Atoi(n)
+				if parzial == 0 {
+					parzial = num
+				} else {
+					parzial = parzial * num
+				}
 			}
 		}
+		// fmt.Println(parzial)
+		total = total + parzial
 	}
-	return checkedIng
+
+	return total
+}
+
+func parseFile(scanner *bufio.Scanner) ([][]string, []string) {
+	var numbers [][]string
+	var operation []string
+	count := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+		r1 := regexp.MustCompile(`\d+`)
+		r2 := regexp.MustCompile(`[+]|[*]`)
+		rowNumbers := r1.FindAllString(line, -1)
+		rowOperations := r2.FindAllString(line, -1)
+		if len(rowNumbers) != 0 {
+			numbers = append(numbers, rowNumbers)
+		} else {
+			operation = rowOperations
+		}
+		count++
+	}
+	return numbers, operation
+}
+
+func transpose(matrix [][]string) [][]string {
+	m := len(matrix)
+	n := len(matrix[0])
+
+	newMatrix := make([][]string, n)
+	for i := 0; i < n; i++ {
+		newMatrix[i] = make([]string, m)
+	}
+
+	var row []string
+	for i := 0; i < n; i++ {
+		row = newMatrix[i]
+		for j := 0; j < m; j++ {
+			row[j] = matrix[j][i]
+		}
+	}
+
+	return newMatrix
 }
